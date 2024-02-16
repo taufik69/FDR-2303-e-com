3724eefc4915803b6cdf802d199e2b6c7c07e2f7
Taufik islam <97851815+taufik69@users.noreply.github.com>
2024-02-16 19:53:40 +0600

    banner and develry section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../../Component/Header/Header";
 import MenuBar from "../../Component/MenuBar/MenuBar";
+import Banner from "../../Component/Banner/Banner";
+import Shipping from "../../Component/Shipping/Shipping";
 const Home = () => {
   return (
     <div>
       <Header />
       <MenuBar />
+      <Banner />
+      <Shipping />
     </div>
   );
 };
