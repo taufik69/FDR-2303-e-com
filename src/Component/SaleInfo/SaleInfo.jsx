64da702b18c0e6fd5e8413e51e94231d8905d306
Taufik islam <97851815+taufik69@users.noreply.github.com>
2024-02-17 19:10:38 +0600

    transfer file

--- src/Component/SaleInfo/SaleInfo.jsx
+++ src/Component/SaleInfo/SaleInfo.jsx
@@ -15,22 +15,7 @@ const SaleInfo = () => {
               <picture>
                 <img src={saleLeft} alt={saleLeft} />
               </picture>
-              <div className=" absolute bottom-16  left-16">
-                <h3 className="text-main_font_color font-bold font-DMsans text-[39px]">
-                  Phones Sale
-                </h3>
-                <p className="mt-12 text-main_font_color font-normal font-DMsans text-base opacity-75 mb-12">
-                  Up to{" "}
-                  <span className="font-bold text-4xl align-middle">30% </span>{" "}
-                  sale for all phones!
-                </p>
-                <Button
-                  className={
-                    "py-4 px-14 bg-main_font_color text-main_bg_color hover:bg-slate-700"
-                  }
-                  title="Shop Now"
-                />
-              </div>
+              
             </div>
             <div className="w-full ">
               <Flex className={"flex-col gap-y-11"}>
