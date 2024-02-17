ed593cb0676800a3c6615d2f01a1213138c602a8
Taufik islam <97851815+taufik69@users.noreply.github.com>
2024-02-17 19:51:11 +0600

    ovelay component done

--- src/Component/CommonConponent/Overlay.jsx
+++ src/Component/CommonConponent/Overlay.jsx
@@ -1,21 +1,31 @@
 import React from "react";
-
-const Overlay = () => {
+import Button from "./Button";
+const Overlay = ({
+  className,
+  title,
+  discountDetilsLast,
+  btnTitle,
+  discountDetilsFast,
+  discountAmount,
+}) => {
   return (
     <>
-      <div className=" absolute bottom-16  left-16">
+      <div className={`absolute ${className}`}>
         <h3 className="text-main_font_color font-bold font-DMsans text-[39px]">
-          Phones Sale
+          {title ? title : "Phones Sale"}
         </h3>
         <p className="mt-12 text-main_font_color font-normal font-DMsans text-base opacity-75 mb-12">
-          Up to <span className="font-bold text-4xl align-middle">30% </span>{" "}
-          sale for all phones!
+          {discountDetilsFast}
+          <span className="font-bold text-4xl align-middle">
+            {discountAmount}
+          </span>
+          {discountDetilsLast}
         </p>
         <Button
           className={
             "py-4 px-14 bg-main_font_color text-main_bg_color hover:bg-slate-700"
           }
-          title="Shop Now"
+          title={btnTitle}
         />
       </div>
     </>
--- src/Component/SaleInfo/SaleInfo.jsx
+++ src/Component/SaleInfo/SaleInfo.jsx
@@ -3,7 +3,7 @@ import Flex from "../CommonConponent/Flex.jsx";
 import saleLeft from "../../assets/Image1.png";
 import electronic from "../../assets/electronicImage.png";
 import FurnitureImage from "../../assets/FurnitureImage.png";
-import Button from "../CommonConponent/Button.jsx";
+import Overlay from "../CommonConponent/Overlay.jsx";
 
 const SaleInfo = () => {
   return (
@@ -15,19 +15,42 @@ const SaleInfo = () => {
               <picture>
                 <img src={saleLeft} alt={saleLeft} />
               </picture>
-              
+              <Overlay
+                className={"bottom-16  left-16"}
+                title="Phones Sale"
+                discountDetilsFast={` Up to`}
+                discountDetilsLast={` sale for all phones!`}
+                discountAmount="30%"
+                btnTitle="Shop Now"
+              />
             </div>
             <div className="w-full ">
               <Flex className={"flex-col gap-y-11"}>
-                <div className="w-full ">
+                <div className="w-full relative ">
                   <picture>
                     <img src={electronic} alt={electronic} />
                   </picture>
+                  <Overlay
+                    className={" top-1/2 -translate-y-1/2  left-16"}
+                    title="Electronics Sale"
+                    discountDetilsFast={` Up to`}
+                    discountDetilsLast={`sale for all electronics! `}
+                    discountAmount="70%"
+                    btnTitle="Shop Now"
+                  />
                 </div>
-                <div className="w-full  ">
+                <div className="w-full  relative ">
                   <picture>
                     <img src={FurnitureImage} alt={FurnitureImage} />
                   </picture>
+                  <Overlay
+                    className={" top-1/2 -translate-y-1/2  left-16"}
+                    title="Furniture Offer"
+                    discountDetilsFast={` Up to`}
+                    discountDetilsLast={`sale for all electronics! `}
+                    discountAmount="50%"
+                    btnTitle="Shop Now"
+                  />
                 </div>
               </Flex>
             </div>
