10b85bf533b46ff1b70fb1846ff7c813622dde67
Taufik islam <97851815+taufik69@users.noreply.github.com>
2024-03-02 19:51:51 +0600

    something modified

--- src/Component/HomeComponent/Header/Header.jsx
+++ src/Component/HomeComponent/Header/Header.jsx
@@ -1,19 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../../../assets/Logo.png";
 import Flex from "../../CommonConponent/Flex";
+import { FaBars } from "react-icons/fa";
 const Header = () => {
+  const [showNav, setshowNav] = useState(true);
+  const [togglebtn, setTogglebtn] = useState(true);
+  const HandleBar = () => {
+    setTogglebtn(!togglebtn);
+  };
+
+  useEffect(() => {
+    const resiezeWindow = () => {
+      console.log(window.innerWidth);
+      if (window.innerWidth > 800) {
+        setshowNav(true);
+      } else {
+        setshowNav(false);
+      }
+    };
+
+    window.addEventListener("resize", resiezeWindow);
+  }, [showNav]);
   return (
     <>
       <div className="py-8 bg-main_bg_color">
-        <div className="container">
+        <div className="container ">
           <Flex className="justify-between items-center ">
             <div>
               <picture>
                 <img src={Logo} alt={Logo} />
               </picture>
             </div>
+            {showNav && (
+              <div>
+                <Flex className={" flex flex-row gap-x-10"}>
+                  <li>
+                    <a
+                      href="#"
+                      className="menuItem transition-all hover:text-main_font_color hover:font-bold"
+                    >
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="menuItem  ">
+                      Shop
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="menuItem  ">
+                      About
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="menuItem ">
+                      Contacts
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="menuItem ">
+                      Journal
+                    </a>
+                  </li>
+                </Flex>
+              </div>
+            )}
+            <div className="" onClick={HandleBar}>
+              <FaBars />
+            </div>
+          </Flex>
+
+          {togglebtn && (
             <div>
-              <Flex className={"gap-x-10"}>
+              <Flex className={" flex-col gap-x-10"}>
                 <li>
                   <a
                     href="#"
@@ -44,8 +103,7 @@ const Header = () => {
                 </li>
               </Flex>
             </div>
-            <div></div>
-          </Flex>
+          )}
         </div>
       </div>
     </>
