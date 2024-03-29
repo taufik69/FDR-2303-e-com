d5d9aafee0e329fd91658e14ee90f98ad2dc9dfe
taufik69 <taufik35-2915@diu.edu.bd>
2024-03-29 15:46:00 +0600

    Newly create menu

--- src/Component/HomeComponent/MenuBar/MenuBar.jsx
+++ src/Component/HomeComponent/MenuBar/MenuBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Flex from "../../CommonConponent/Flex";
 import { HiOutlineBars3BottomLeft } from "react-icons/hi2";
 import Serach from "../../CommonConponent/Serach";
@@ -7,34 +7,9 @@ import { BiChevronDown } from "react-icons/bi";
 import { BiChevronUp } from "react-icons/bi";
 
 const MenuBar = () => {
-  const [shopCatagory, setShopCatagory] = useState(false);
-  const [HumanCatagoriesShow, setHumanCatagoriesShow] = useState(false);
-  const [cartCatagoryShow, setcartCatagoryShow] = useState(false);
-
-  const HandleClick = () => {
-    if (HumanCatagoriesShow) {
-      setHumanCatagoriesShow(false);
-    }
-    setShopCatagory(!shopCatagory);
-  };
-
-  // HanldeAcount funtionality
-  const HanldeAcount = (e) => {
-    e.stopPropagation();
-    if (shopCatagory) {
-      setShopCatagory(false);
-    }
-    setHumanCatagoriesShow(!HumanCatagoriesShow);
-  };
-
-  // HandleCart funtionality
-  const HandleCart = (e) => {
-    e.stopPropagation();
-    if (shopCatagory || HumanCatagoriesShow) {
-      setHumanCatagoriesShow(false);
-      setShopCatagory(false);
-    }
-    setcartCatagoryShow(!cartCatagoryShow);
+  // HandleBar funtionality implementation
+  const HandleBar = () => {
+    alert("aklshdf");
   };
   return (
     <>
@@ -42,80 +17,33 @@ const MenuBar = () => {
         <div className="container">
           <Flex className={"gap-x-3 justify-between items-center"}>
             <Flex className={"gap-x-2"}>
-              <div onClick={HandleClick}>
+              <div>
                 <HiOutlineBars3BottomLeft
-                  className={`text-2xl cursor-pointer hover:scale-105 transition ${
-                    shopCatagory && "text-red-300"
-                  }`}
+                  className="cursor-pointer tex-4xl"
+                  onClick={HandleBar}
                 />
               </div>
               <p className="hidden md:block menuItem cursor-pointer text-main_font_color">
                 Shop by Category
               </p>
-
-              {shopCatagory && (
-                <ul
-                  className={`absolute  ${
-                    shopCatagory
-                      ? "top-40 text-center  bg-red-300 z-10 w-full rounded-sm md:w-1/3"
-                      : null
-                  }`}
-                >
-                  {[...new Array(5)].map((_, id) => (
-                    <li key={id}>Home</li>
-                  ))}
-                </ul>
-              )}
             </Flex>
             <Serach placeHolder="Search Products" />
 
             <Flex className={"gap-x-2 sm:gap-x-5"}>
-              <div onClick={HanldeAcount}>
+              <div>
                 <Flex
                   className={"gap-x-1 sm:gap-x-2 cursor-pointer items-center"}
                 >
                   <div>
-                    <FaUser
-                      className={`${
-                        HumanCatagoriesShow
-                          ? "text-blue-500 scale-105"
-                          : "text-black-500"
-                      }`}
-                    />
+                    <FaUser />
                   </div>
                   <div>
-                    {HumanCatagoriesShow ? <BiChevronUp /> : <BiChevronDown />}
+                    <BiChevronDown />
                   </div>
                 </Flex>
-                {HumanCatagoriesShow && (
-                  <ul
-                    className={`absolute  ${
-                      HumanCatagoriesShow
-                        ? "top-40 text-center left-0 bg-blue-300 z-10 w-full rounded-sm md:w-1/3"
-                        : null
-                    }`}
-                  >
-                    {[...new Array(3)].map((_, id) => (
-                      <li key={id}>HumanCatagoryo</li>
-                    ))}
-                  </ul>
-                )}
               </div>
-              <div className="cursor-pointer" onClick={HandleCart}>
+              <div className="cursor-pointer">
                 <FaShoppingCart />
-                {cartCatagoryShow && (
-                  <ul
-                    className={`absolute  ${
-                      cartCatagoryShow
-                        ? "top-40 text-center right-0 bg-purple-300 z-10 w-full rounded-sm md:w-1/3"
-                        : null
-                    }`}
-                  >
-                    {[...new Array(3)].map((_, id) => (
-                      <li key={id}>cart</li>
-                    ))}
-                  </ul>
-                )}
               </div>
             </Flex>
           </Flex>
