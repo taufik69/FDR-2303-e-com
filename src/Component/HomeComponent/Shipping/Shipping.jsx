12120c9c13bc83f57a31aaeef6742308926f2121
taufik69 <97851815+taufik69@users.noreply.github.com>
2024-04-01 22:10:42 +0600

    shipping part responsive done

--- src/Component/HomeComponent/Shipping/Shipping.jsx
+++ src/Component/HomeComponent/Shipping/Shipping.jsx
@@ -2,21 +2,28 @@ import React, { useEffect, useState } from "react";
 import Flex from "../../CommonConponent/Flex";
 import ShipingItem from "../../CommonConponent/ShipingItem";
 import { FaTruck } from "react-icons/fa6";
-
+import { SlReload } from "react-icons/sl";
+import { PiFlowArrowDuotone } from "react-icons/pi";
 const Shipping = () => {
   const data = [
-    { id: 1, icon: <FaTruck />, title: "Free shipping" },
+    { id: 1, icon: <PiFlowArrowDuotone />, title: "Two years warranty" },
     { id: 1, icon: <FaTruck />, title: "Return policy in 30 days" },
-    { id: 1, icon: <FaTruck />, title: "Free shipping" },
+    { id: 1, icon: <SlReload />, title: "Free shipping" },
   ];
   return (
     <>
       <div className="mt-5 px-5  px-sm-0 ">
         <div className="container">
-          <Flex className={"justify-between items-center"}>
+          <Flex
+            className={
+              "flex-col sm:flex-row gap-y-3 justify-between items-center"
+            }
+          >
             {data?.map((item) => (
               <ShipingItem
-                className={"flex gap-x-3 items-center text-[13px]"}
+                className={
+                  "flex gap-x-3 items-center sm:text-[13px] cursor-pointer"
+                }
                 key={item.id}
               >
                 <span>{item.icon}</span>
