4ce0dd363a9f249763166eba6fbb1bbba7652abd
taufik69 <taufik35-2915@diu.edu.bd>
2024-05-04 18:12:28 +0600

    test branch

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-<<<<<<< HEAD
-import { store } from "./Store/Store.js";
-=======
 import { store } from "./Redux/Store/Store.js";
->>>>>>> main
 import { Provider } from "react-redux";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
