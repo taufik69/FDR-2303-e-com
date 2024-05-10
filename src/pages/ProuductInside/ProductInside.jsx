970d88436447f4c8b987d459604c731fcd1c2594
taufik69 <taufik35-2915@diu.edu.bd>
2024-05-10 18:12:43 +0600

    test branch

--- src/pages/ProuductInside/ProductInside.jsx
+++ src/pages/ProuductInside/ProductInside.jsx
@@ -5,11 +5,7 @@ import { FetcherProduct } from "../../Redux/AllSlice/ProductSlice/ProductSlice";
 
 const ProductInside = () => {
   const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(FetcherProduct("https://jsonplaceholder.typicode.com/todos"));
-  }, []);
-  const { data, status } = useSelector((state) => state.prouduct);
-  console.log(data);
+
   return (
     <div className="py-10">
       <div className="container">
@@ -17,9 +13,7 @@ const ProductInside = () => {
       </div>
       <div>
         <div className="max-w-[50%]">
-          <picture>
-            <img src="" alt="" />
-          </picture>
+          <picture>{start}</picture>
         </div>
       </div>
     </div>
