35af64e0e87958413e9cb5b74aee39f7b832b56c
taufik69 <taufik35-2915@diu.edu.bd>
2024-07-07 19:44:56 +0600

    catagories and sub catagoris done

--- src/Component/CommonConponent/ShopLeftitem/ShopCatagories.jsx
+++ src/Component/CommonConponent/ShopLeftitem/ShopCatagories.jsx
@@ -4,37 +4,37 @@ import Flex from "../Flex";
 
 const ShopCatagories = ({ catagoresData, shopLeftTitle }) => {
   const [showCatagories, setshowCatagories] = useState(
-    catagoresData.map(() => false)
+    catagoresData?.map(() => !true || false),
   );
 
   const HanldeCatagoryToggle = (idx) => {
     setshowCatagories((previousState) => {
       return previousState.map((value, index) =>
-        idx === index ? !value : false
+        idx === index ? !value : false,
       );
     });
   };
-
+  console.log(showCatagories);
   return (
     <>
       <div>
-        <h1 className="font-DMsans font-bold text-xl mb-9">
+        <h1 className="mb-9 font-DMsans text-xl font-bold">
           {shopLeftTitle ? shopLeftTitle : "Title is Missing"}
         </h1>
       </div>
 
       <div>
-        <div className="divide-y-2 divide-[#F0F0F0] divide-solid  ">
+        <div className="divide-y-2 divide-solid divide-[#F0F0F0]  ">
           {catagoresData?.map((item, index) =>
-            item.subcatgores.length > 0 ? (
+            item.subcategories.length > 0 ? (
               <div>
                 <div
-                  className="py-5 px-2 cursor-pointer"
+                  className="cursor-pointer px-2 py-5"
                   key={item.id}
                   onClick={() => HanldeCatagoryToggle(index)}
                 >
-                  <Flex className={"justify-between items-center "}>
-                    <h2 className="text-secondary_font_color font-normal text-base ">
+                  <Flex className={"items-center justify-between "}>
+                    <h2 className="text-base font-normal text-secondary_font_color ">
                       {item.title}
                     </h2>
                     <FaPlus className="text-secondary_font_color" />
@@ -43,25 +43,28 @@ const ShopCatagories = ({ catagoresData, shopLeftTitle }) => {
 
                 <div className={`${showCatagories[index] && "mb-4"}`}>
                   {showCatagories[index] &&
-                    item.subcatgores.map((subitem) => (
-                      <div
-                        className="px-3 py-2 cursor-pointer"
-                        key={subitem.id}
-                      >
-                        <h2 className="text-secondary_font_color font-normal text-base ">
-                          {subitem.title}
-                        </h2>
-                      </div>
-                    ))}
+                    item.subcategories.map(
+                      (subitem) =>
+                        subitem !== item.title && (
+                          <div
+                            className="cursor-pointer px-3 py-2"
+                            key={subitem}
+                          >
+                            <h2 className="text-base font-normal text-secondary_font_color ">
+                              {subitem}
+                            </h2>
+                          </div>
+                        ),
+                    )}
                 </div>
               </div>
             ) : (
-              <div className="py-5 px-2 cursor-pointer">
-                <h2 className="text-secondary_font_color font-normal text-base ">
+              <div className="cursor-pointer px-2 py-5">
+                <h2 className="text-base font-normal text-secondary_font_color ">
                   {item.title}
                 </h2>
               </div>
-            )
+            ),
           )}
         </div>
       </div>
--- src/Component/ShopComponent/ShopLeft/ShopLeft.jsx
+++ src/Component/ShopComponent/ShopLeft/ShopLeft.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   catagories,
   shopByColor,
@@ -7,11 +7,49 @@ import {
 } from "../../../../Data/Data.js";
 import ShopCatagories from "../../CommonConponent/ShopLeftitem/ShopCatagories";
 import ShopByColor from "../../CommonConponent/ShopLeftitem/ShopByColor.jsx";
+import { useSelector } from "react-redux";
 const ShopLeft = ({ className }) => {
+  const [allProducts, setallProducts] = useState([]);
+  const { data, status } = useSelector((state) => state.prouduct);
+  useEffect(() => {
+    if (status.payload === "IDLE") {
+      setallProducts(data.payload.products);
+    }
+  }, [status.payload, data.payload]);
+
+  let Catagoryarr = [];
+  let categoriesSet = new Set();
+
+  allProducts.forEach((product) => {
+    if (!categoriesSet.has(product.category)) {
+      Catagoryarr.push({
+        id: product.id,
+        title: product.category,
+        subcategories: product.tags,
+      });
+      categoriesSet.add(product.category);
+    }
+  });
+
+  let Brandarr = [];
+  let BrandarrSet = new Set();
+
+  allProducts.forEach((product) => {
+    if (!BrandarrSet.has(product.brand)) {
+      Brandarr.push({
+        id: product.id,
+        title: product.brand,
+      });
+      categoriesSet.add(product.brand);
+    }
+  });
+
+  console.log(Brandarr);
+
   return (
     <div className={className}>
       <ShopCatagories
-        catagoresData={catagories ? catagories : []}
+        catagoresData={Catagoryarr ? Catagoryarr : []}
         shopLeftTitle={"Shop by Category"}
       />
       <ShopByColor
@@ -23,7 +61,7 @@ const ShopLeft = ({ className }) => {
       />
       <ShopByColor
         shopLeftTitle={"Shop by Brand"}
-        shopCatagoryItem={shopByBrand ? shopByBrand : []}
+        shopCatagoryItem={Brandarr ? Brandarr : []}
         color={false}
         dropdownis={true}
         dropdownExpandIs={false}
